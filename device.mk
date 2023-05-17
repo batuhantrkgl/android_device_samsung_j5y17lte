c5cc5da5ff22a1a07729a1f96467eb80609fe54a
flominatorgd <github.flominatorgd@gmail.com>
2023-05-17 18:12:47 +0200

    j5y17lte: set WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true & bringup and built android.hardware.bluetooth@1.0-service-qti.a3y17lte and remove unused android.hardware.bluetooth@1.0-impl

--- device.mk
+++ device.mk
@@ -39,7 +39,8 @@ PRODUCT_AAPT_PREBUILT_DPI := xhdpi hdpi
 # Bluetooth hal
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl.7870 \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0-service \
+    android.hardware.bluetooth@1.0-service-qti.a3y17lte
 
 # Bluetooth audio
 PRODUCT_PACKAGES += \
