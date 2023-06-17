78d3dd347d78b9a1b39a017a11370894ff0f2bec
Batuhantrkgl <batuhanturkoglu37@gmail.com>
2023-06-17 21:57:49 +0300

    Add missing files

--- arrow_j5y17lte.mk
+++ arrow_j5y17lte.mk
@@ -28,11 +28,7 @@ $(call inherit-product, vendor/arrow/config/common.mk)
 # Device identifier. This must come after all inclusions
 <<<<<<<< HEAD:havoc_j5y17lte.mk
 PRODUCT_DEVICE := j5y17lte
-PRODUCT_NAME := havoc_j5y17lte
-========
-PRODUCT_DEVICE := a3y17lte
-PRODUCT_NAME := arrow_a3y17lte
->>>>>>>> 023cac2 (declare use of oss audio and uprev to android.hardware.soundtrigger@2.3-impl, android.hardware.audio.effect@6.0-impl, android.hardware.audio@6.0-impl, add device speciffic wifi overlay, add fix for hotspot on android 11 for qcom wifi by setting WIFI_AVOID_IFACE_RESET_MAC_CHANGE := true, add missing android.hardware.nfc.uicc.xml, Add parameters for Hotspot 2.0 in wpa_supplicant_overlay.conf, refractor for arrow):arrow_a3y17lte.mk
+PRODUCT_NAME := arrow_j5y17lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := Galaxy J5 2017
 PRODUCT_MANUFACTURER := samsung
